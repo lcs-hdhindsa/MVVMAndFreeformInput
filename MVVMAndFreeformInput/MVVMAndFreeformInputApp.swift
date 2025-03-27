39d6003cbff9cb52d64a2100b16359661bcb0730
Harshan Dhindsa <hdhindsa@lcs.on.ca>
2025-03-26 20:37:21 -0400

    Initial Commit

---
+++ MVVMAndFreeformInput/MVVMAndFreeformInputApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMAndFreeformInputApp.swift
+//  MVVMAndFreeformInput
+//
+//  Created by Harshan Dhindsa on 2025-03-26.
+//
+
+import SwiftUI
+
+@main
+struct MVVMAndFreeformInputApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
